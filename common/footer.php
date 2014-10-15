a4f3c14e865d1b1bda22aa603ae6fbee7b78b318
Taromi <lovetip3@gmail.com>
2014-10-15 19:42:06 +0900

    푸터 변경
    회사명 변경

--- common/footer.php
+++ common/footer.php
@@ -1,6 +1,6 @@
       <!-- FOOTER -->
       <footer><br /><br />
-        <p align="center">&copy; 2014 The Chef(Ellestra) Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+        <p align="center">&copy; 2014 Estrella Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
       </footer>
 
     <!-- Bootstrap core JavaScript
