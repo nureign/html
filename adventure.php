9960c0b77d69323f402dadbbac96e90165273805
Taromi <lovetip3@gmail.com>
2014-10-15 19:41:42 +0900

    [비반응형 시작] 모험하기

--- adventure.php
+++ adventure.php
@@ -5,29 +5,27 @@
 	
 	include_once "./common/header.php";
 	include_once "./common/nav.php";
-	//include_once "./common/db_conn.php";
+	include_once "./common/db_conn.php";
 ?>
-<audio src="./bgm/adventure_bg.mp3" autoplay="autoplay" loop="loop">
-</audio>
+<audio src="./bgm/adventure_bg.mp3" autoplay="autoplay" loop="loop"></audio>
 <!-- 윗 상단 로우 전체 시작 -->
 <style type="text/css">
 body {
 
 	background-image: url(images/bgleft.gif), url(images/bgright.gif); 
-   background-repeat: repeat-y; 
-   background-position: top left, top right; 
+	background-repeat: repeat-y; 
+	background-position: top left, top right; 
 }
 </style>
 
 <div class="row" align="center">
-	<div class="col-md-2"><!--여백--></div>
+	<div class="col-xs-1"><!--여백--></div>
 
     <!-- 중간부분 정렬로우 -->
-	<div class="col-md-8">
-     
-     	<div class="well well-sm"><h2>모험을 떠나왔다</h2>행성이름 : 갤슈아 행성<br />위치 : 목초지</div>
-         <!-- 모험하기 시작 -->
-		<div class="panel panel-info">
+	<div class="col-cs-10">
+		<!-- 모험하기 시작 -->
+     	<div class="well well-sm width"><h2>모험을 떠나왔다</h2>행성이름 : 갤슈아 행성<br />위치 : 목초지</div>
+		<div class="panel panel-info width">
             <div class="panel-heading">특수아이템 발동</div>
             <div class="panel-body" align="center">
             	<h5>소지하고 있던 의문의 넥클레스에 의해 상대방에게 의문의 저주를 겁니다.</h5>
@@ -37,49 +35,45 @@ body {
     </div>
     <!-- 중간부분 정렬로우 종료 -->
 
-	<div class="col-md-2"><!-- 공백 --></div>
+	<div class="col-xs-1"><!-- 공백 --></div>
 </div>
 <!-- 윗 상단 로우 전체 닫기 -->
 
 
-
-
 <!-- 중간 로우 전체 시작 -->
 <div class="row" align="center">
-	<div class="col-md-2"><!--여백--></div>
+	<div class="col-xs-1"><!--여백--></div>
 
-    <!-- 중간부분 왼쪽로우 시작-->
-	<div class="col-md-4">
-        <table class="table table-bordered">
+    <!-- 중간부분 시작-->
+	<div class="col-xs-10">
+        <table class="table table-bordered width">
             <tr>
-              <td>
-              	<img src="./images/char1.gif" alt="..." width="170" height="170" class="img-rounded" data-src="holder.js/300x300">
-              </td>
               <td align="center">
+              	<!-- 나의 케릭터 현황 -->
+              	<img src="./images/char1.gif" alt="..." width="170" height="170" class="img-rounded" data-src="holder.js/300x300">
+                <!-- 나의 케릭터 현황 닫기 -->
+              </td>
+              <td align="center" width="380px">
               
                 <!-- 체력부분 열기 -->
                 <div class="input-group">
                     <span class="input-group-addon"><span class="label label-danger">체력</span></span>
                         <div class="progress, form-control">
-                            <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">
-                            100%
-                            </div>
+                        <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">100%</div>
                         </div>
                         <span class="input-group-addon">5000/5000</span>
-                </div>
+                		</div>
                 <!-- 체력부분 닫기 -->
                 
-		<!-- 멘탈부분 열기 -->
-        <div class="input-group">
-  			<span class="input-group-addon"><span class="label label-warning">멘탈</span></span>
-  				<div class="progress, form-control">
-					<div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 99%;">
-					99%
-					</div>
-				</div>
-                <span class="input-group-addon">1980/2000</span>
-		</div>
-        <!-- 멘탈부분 닫기 -->
+                <!-- 멘탈부분 열기 -->
+                <div class="input-group">
+                    <span class="input-group-addon"><span class="label label-warning">멘탈</span></span>
+                        <div class="progress, form-control">
+                        <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 99%;">99%</div>
+                        </div>
+                        <span class="input-group-addon">1980/2000</span>
+                        </div>
+                <!-- 멘탈부분 닫기 -->
         
         		<!-- 스킬 사용 부분 시작 -->
         		<br />
@@ -112,62 +106,48 @@ body {
                 </div>
 				<!-- 아이템 사용 부분 종료 -->
               </td>
-            </tr>
-        </table>
-    </div>
-    <!-- 중간부분 왼쪽로우 종료 -->
-    
-    
-    
-    
-    <!-- 중간부분 오른쪽로우 시작-->
-	<div class="col-md-4">
-        <table class="table table-bordered">
-            <tr>
-              <td>
-              	<img src="./images/mob_goblin.gif" alt="..." width="170" height="170" class="img-rounded" data-src="holder.js/300x300">
+              <td align="center">
+              	<!-- 모험의 적 이미지 표시 -->
+				<img src="./images/mob_goblin.gif" alt="..." width="170" height="170" class="img-rounded" data-src="holder.js/300x300">
+                <!-- 모험의 적 이미지 닫기 -->
               </td>
-              <td>
-              
+              <td align="center" width="380px">
 				<!-- 체력부분 열기 -->
                 <div class="input-group">
                     <span class="input-group-addon"><span class="label label-danger">체력</span></span>
                         <div class="progress, form-control">
-                            <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">
-                            100%
-                            </div>
+                        <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">100%</div>
                         </div>
                         <span class="input-group-addon">5000/5000</span>
-                </div>
+                		</div>
                 <!-- 체력부분 닫기 -->
-               
- 
+            
+            <!-- 모험의 적 이름, 속성 자원형태 표시부분 -->   
             <ul class="list-group">
 			  <li class="list-group-item">이름 : 고블린전사</li>
               <li class="list-group-item">속성 : 물리타입</li>
               <li class="list-group-item">자원형태 : 골드</li>
             </ul>
-
+            <!-- 모험의 적 이름, 속성, 자원형태 표시 닫기 -->
               </td>
             </tr>
         </table>
     </div>
-    <!-- 중간부분 오른쪽로우 종료 -->
+    <!-- 중간부분 종료 -->
 
-	<div class="col-md-2"><!-- 공백 --></div>
+	<div class="col-xs-1"><!-- 공백 --></div>
 </div>
 <!-- 중간 로우 전체 닫기 -->
 
 
-
-
 <!-- 아래 로우 전체 시작 -->
 <div class="row" align="center">
-	<div class="col-md-2"><!--여백--></div>
+	<div class="col-xs-1"><!--여백--></div>
 
     <!-- 중간부분 로우 시작-->
-	<div class="col-md-8">
-        <div class="panel panel-default">
+	<div class="col-xs-10">
+    	<!-- 전투 로직 디자인 시작 -->
+        <div class="panel panel-default width">
           <div class="panel-body">
             <ul class="list-group">
             	<li class="list-group-item">모에모에 이(/가) 고블린에게 도전하였다!<br />소지하고 있던 엑스칼리버로 적을 두동강 냈다! 적은 13500 데미지를 입었다!</li>
@@ -181,17 +161,15 @@ body {
             <button type="button" class="btn btn-default">전투마무리</button>
           </div>
         </div>
+        <!-- 전투 로직 디자인 종료 -->
     </div>
     <!-- 중간부분 로우 종료 -->
 
-	<div class="col-md-2"><!-- 공백 --></div>
+	<div class="col-xs-1"><!-- 공백 --></div>
 </div>
 <!-- 아래 로우 전체 닫기 -->
-
-
 	<?
 		include_once "./common/footer.php";
 	?>
-
   </body>
 </html>
--- common/header.php
+++ common/header.php
@@ -7,14 +7,15 @@
 <title>Estrella Webgame</title>
     <link rel="stylesheet" href="./css/bootstrap.css" type="text/css" media="screen" title="no title" />
     <link rel="stylesheet" href="./css/font-awesome.css" type="text/css" media="screen" title="no title" /> 
-     <meta charset="utf8">
+    <meta charset="utf8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" href="../../favicon.ico">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
     <script src="js/bootstrap.js"></script> 
         <!-- Custom styles for this template -->
-    <link href="navbar-static-top.css" rel="stylesheet">
+    <link href="./css/navbar-static-top.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="./css/non-responsive.css" rel="stylesheet">
 </head>
