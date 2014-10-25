e143c31df804ed7f4ad46cc5315981dc1d68a724
Taromi <lovetip3@gmail.com>
2014-10-25 20:30:32 +0900

    자유토론 게시판 디자인 완성
    자유토론 게시판 디자인 완성
    1. 목록화면 (리스트)
    2. 글쓰기
    3. 글읽기

---
+++ freebbs_list.php
@@ -0,0 +1,100 @@
+<?
+	/*
+		freebbs_list.php (자유토론 게시판 - 리스트부분)
+	*/
+	
+	include_once "./common/header.php";
+	include_once "./common/nav.php";
+	include_once "./common/db_conn.php";
+
+/*	$id = $_POST['id'];
+
+	if( !isset($id) )
+		exit;
+*/
+
+
+/*
+	로그인이 되어 있지 않은지 반드시 체크해야 함
+	
+	로그인이 되어 있지 않은지 반드시 체크해야 함
+	
+	로그인이 되어 있지 않은지 반드시 체크해야 함
+	
+	로그인이 되어 있지 않은지 반드시 체크해야 함
+	
+	로그인이 되어 있지 않은지 반드시 체크해야 함
+	
+	로그인이 되어 있지 않은지 반드시 체크해야 함
+	
+	- 비회원은 읽기 쓰기 모두 못하게....
+	
+*/
+
+?>
+<!-- 로우 전체 시작 -->
+<div class="row" align="center">
+	<div class="col-xs-1"><!--여백--></div>
+
+    <!-- 자유토론 시작 -->
+	<div class="col-xs-10">
+	<h2 align="center">자유토론 커뮤니티</h2><br />
+	<table class="table table-bordered" style="width:900px;">
+    	<tr align="center" style="background-color:#F3F3F3;">
+        	<td>번호</td>
+            <td>제목</td>
+            <td>케릭명</td>
+            <td>날짜</td>
+        </tr>
+        <tr>
+        	<td align="center" width="100px">공지</td>
+            <td width="350px">에스트렐라 자유토론 게시판입니다.</td>
+            <td align="center" width="150px">어스GM</td>
+            <td align="center" width="100px">14-10-25 19:25</td>
+        </tr>
+        <tr>
+        	<td align="center" width="100px">공지</td>
+            <td width="350px">게시판 이용규칙 (V1.0)</td>
+            <td align="center" width="150px">어스GM</td>
+            <td align="center" width="100px">14-10-25 20:00</td>
+        </tr>
+        <tr>
+        	<td align="center" width="100px">12032</td>
+            <td><a href="./freebbs_read.php">천제의 고요 퀘스트 도와주실분?</a></td>
+            <td align="center" width="150px">갤럭시뿅뿅</td>
+            <td align="center" width="100px">14-10-20 15:44</td>
+        </tr>
+        <tr>
+        	<td align="center" colspan="4">
+            <form class="form-inline" role="form">
+				<div class="form-group">
+                    <select class="form-control">
+                      <option>제목</option>
+                      <option>내용</option>
+                      <option>케릭명</option>
+                    </select>
+				</div>
+				<div class="form-group">
+                <input type="text" class="form-control" i	d="whatareuwant" placeholder="무엇을 찾으시겠어요?">
+                </div>
+				<button type="submit" class="btn btn-default">검색</button>
+                <a href="./freebbs_write.php"><button type="button" class="btn btn-info">글쓰기</button></a>
+            </form>
+            
+            </td>
+         </tr>
+      </td>
+    </table><br />
+	</div>     
+    <!-- 자유토론 닫기 -->
+
+	<div class="col-xs-1"><!-- 공백 --></div>
+</div>
+<!-- 로우 전체 닫기 -->
+
+	<?
+		include_once "./common/footer.php";
+	?>
+
+  </body>
+</html>
---
+++ freebbs_read.php
@@ -0,0 +1,84 @@
+<?
+	/*
+		freebbs_read.php (자유토론 게시판 - 읽기부분)
+	*/
+	
+	include_once "./common/header.php";
+	include_once "./common/nav.php";
+	include_once "./common/db_conn.php";
+
+/*	$id = $_POST['id'];
+
+	if( !isset($id) )
+		exit;
+*/
+
+
+/*
+	로그인이 되어 있지 않은지 반드시 체크해야 함
+	
+	로그인이 되어 있지 않은지 반드시 체크해야 함
+	
+	로그인이 되어 있지 않은지 반드시 체크해야 함
+	
+	로그인이 되어 있지 않은지 반드시 체크해야 함
+	
+	로그인이 되어 있지 않은지 반드시 체크해야 함
+	
+	로그인이 되어 있지 않은지 반드시 체크해야 함
+	
+	- 비회원은 읽기 쓰기 모두 못하게....
+	
+*/
+
+?>
+<!-- 로우 전체 시작 -->
+<div class="row" align="center">
+	<div class="col-xs-1"><!--여백--></div>
+
+    <!-- 자유토론 읽기 시작 -->
+	<div class="col-xs-10">
+	<h2 align="center">자유토론 커뮤니티</h2><br />
+    <div align="right" class="width" style="margin-right:70px;"><a href="./freebbs_list.php"<button type="button" class="btn btn-warning">게시판으로 돌아가기</button></a></div><br />
+	<table class="table table-bordered" style="width:900px;">
+    	<tr align="center" style="background-color:#F3F3F3;">
+        	<td align="center" width="100px">23423번 내용</td>
+            <td width="350px">천제의 퀘스트 어뜩케 하나용?ㅠ</td>
+            <td align="center" width="150px">갤럭시슝슝</td>
+            <td align="center" width="100px">14-10-25 19:25</td>
+        </tr>
+        <tr>
+        	<td colspan="4" style="padding-left:50px; padding-right:50px; text-align:left;">
+            	<br /><br />사실 천체의 퀘스트? 천제의 퀘스트? 그거 잘 모르겠어요. 지금 레벨은 20인데 너무 퀘스트 달렸나봐요 어케하죠?<br /><br /><br />
+            </td>
+        </tr>
+        <tr>
+        	<td width="50px" align="center">닉네임은닉넴</td>
+            <td colspan="3" width="850px">한줄댓글 입니당... 제가 봤을 땐 조금 더 고렙이 되셔야 할듯여</td>
+        </tr>
+        <tr>
+        	<td colspan="4" align="center">
+            <form class="form-inline" role="form">
+				<div class="form-group">
+                <input type="text" class="form-control" placeholder="한줄댓글 작성" maxlength="50" style="width:550px;">
+                </div>
+				<button type="submit" class="btn btn-default">댓글작성</button>
+            </form>
+            
+            </td>
+         </tr>
+      </td>
+    </table><br />
+	</div>     
+    <!-- 자유토론 읽기 닫기 -->
+
+	<div class="col-xs-1"><!-- 공백 --></div>
+</div>
+<!-- 로우 전체 닫기 -->
+
+	<?
+		include_once "./common/footer.php";
+	?>
+
+  </body>
+</html>
