cae6513a375d36625454dfd6e9b6739799bd71da
Taromi <lovetip3@gmail.com>
2014-10-26 21:54:03 +0900

    행성공략 준비단계 디자인 완성
    1. 누르자마자 300 루비가 소모되어야 함
    2. 음성에 맞게 워프준비 카운트다운이 떨어져야함
    3. 카운트다운이 0이 되었을 때 워프준비 부분이 사라져야 함 (행성공략 글씨만 남게)
    4. 부관이 말하는 작전완료, 행성 조사 완료 부분에 맞게 글자가 표시되게끔 하면 좋겠음....
    5. 체력/멘탈 부분 표시 필요
    6. 내가 현재 소유하고 있는 케릭현황이 보여야함
    7. 1번부터 순서대로 클릭시 2번에는 1번에 선택한 유닛이나 아이템이 안보이게끔 해야함..
    8. 공격을 누르면 planet attack으로 넘어가게됨.

--- planetattack.php
+++ planetattack.php
@@ -1,108 +1,39 @@
 <?
 	/*
-		PlanetAttack.php
+		planetattack.php (행성공략 시작)
 	*/
 	
 	include_once "./common/header.php";
 	include_once "./common/nav.php";
-	//include_once "./common/db_conn.php";
+	include_once "./common/db_conn.php";
+
+	@session_start();
+	
+	$id = $_SESSION['id'];
+/*
+	if( !isset($id) )
+	{
+		echo ("<div align='center'>로그인을 하시기 바랍니다.</div>");
+		exit;
+	}
+*/
 ?>
-
-<!-- 윗 상단 로우 전체 시작 -->
+<!-- 로우 전체 시작 -->
 <div class="row" align="center">
-	<div class="col-md-2"><!--여백--></div>
+	<div class="col-xs-1"><!--여백--></div>
 
-    <!-- 중간부분 정렬로우 -->
-	<div class="col-md-8">
-     
-     	<div class="well well-sm"><h2>모험을 떠나왔다</h2>행성이름 : 갤슈아 행성<br />위치 : 목초지</div>
-         <!-- 모험하기 시작 -->
-		<div class="panel panel-info">
-            <div class="panel-heading">특수아이템 발동</div>
-            <div class="panel-body" align="center">
-            	<h5>소지하고 있던 의문의 넥클레스에 의해 상대방에게 의문의 저주를 겁니다.</h5>
-            </div>
-           	</div>
-		<!-- 모험하기 닫기 -->
-    </div>
-    <!-- 중간부분 정렬로우 종료 -->
+    <!-- 행성공략 본격 시작 -->
+	<div class="col-xs-10">
+	<h2 align="center">자유토론 커뮤니티</h2><br />
+	<table class="table table-bordered" style="width:900px;">
+    	<tr align="center" style="background-color:#F3F3F3;">
+        	
+	</div>     
+    <!-- 행성공략 이제 닫기 -->
 
-	<div class="col-md-2"><!-- 공백 --></div>
+	<div class="col-xs-1"><!-- 공백 --></div>
 </div>
-<!-- 윗 상단 로우 전체 닫기 -->
-
-
-
-
-<!-- 중간 로우 전체 시작 -->
-<div class="row" align="center">
-	<div class="col-md-2"><!--여백--></div>
-
-    <!-- 중간부분 왼쪽로우 시작-->
-	<div class="col-md-4">
-        <table class="table table-bordered">
-            <tr>
-              <td>이미지</td>
-              <td>스텟</td>
-            </tr>
-            <tr>
-            	<td colspan="2">전체 이미지</td>
-            </tr>
-        </table>
-    </div>
-    <!-- 중간부분 왼쪽로우 종료 -->
-    
-    
-    
-    
-    <!-- 중간부분 오른쪽로우 시작-->
-	<div class="col-md-4">
-        <table class="table table-bordered">
-            <tr>
-              <td>이미지</td>
-              <td>스텟</td>
-            </tr>
-            <tr>
-            	<td colspan="2">전체 이미지</td>
-            </tr>
-        </table>
-    </div>
-    <!-- 중간부분 오른쪽로우 종료 -->
-
-	<div class="col-md-2"><!-- 공백 --></div>
-</div>
-<!-- 중간 로우 전체 닫기 -->
-
-
-
-
-<!-- 아래 로우 전체 시작 -->
-<div class="row" align="center">
-	<div class="col-md-2"><!--여백--></div>
-
-    <!-- 중간부분 로우 시작-->
-	<div class="col-md-8">
-        <div class="panel panel-default">
-          <div class="panel-body">
-            <ul class="list-group">
-            	<li class="list-group-item">모에모에 이(/가) 고블린에게 도전하였다!<br />소지하고 있던 엑스칼리버로 적을 두동강 냈다! 적은 13500 데미지를 입었다!</li>
-                <li class="list-group-item">고블린 이(/가) 모에모에에게 강력한 토네이도 발차기를 시전하였다!<br />모에모에는 1300 데미지를 입었다!</li>
-              <li class="list-group-item list-group-item-success">전투에서 승리하였습니다.<br />골드 100,000, 엘릭서 500,000을 획득하였습니다!</li>
-              <li class="list-group-item list-group-item-info">스킬작동!</li>
-              <li class="list-group-item list-group-item-warning">특수아이템 발동!!!!!!</li>
-              <li class="list-group-item list-group-item-danger">아직미정(보스전 활용)</li>
-            </ul>
-            <button type="button" class="btn btn-default">전투포기</button>&nbsp;
-            <button type="button" class="btn btn-default">전투마무리</button>
-          </div>
-        </div>
-    </div>
-    <!-- 중간부분 로우 종료 -->
-
-	<div class="col-md-2"><!-- 공백 --></div>
-</div>
-<!-- 아래 로우 전체 닫기 -->
-
+<!-- 로우 전체 닫기 -->
 
 	<?
 		include_once "./common/footer.php";
--- planetready.php
+++ planetready.php
@@ -1,151 +1,263 @@
 <?
 	/*
-		PlanetReady.php
+		planetattack.php (행성공략 시작)
 	*/
 	
 	include_once "./common/header.php";
 	include_once "./common/nav.php";
-	//include_once "./common/db_conn.php";
+	include_once "./common/db_conn.php";
+
+	@session_start();
+	
+	$id = $_SESSION['id'];
+/*
+	if( !isset($id) )
+	{
+		echo ("<div align='center'>로그인을 하시기 바랍니다.</div>");
+		exit;
+	}
+*/
 ?>
+<style>
+body {
 
-<!-- 윗 상단 로우 전체 시작 -->
+	background-image: url(images/planetready_bg_left.gif), url(images/planetready_bg_right.gif); 
+	background-repeat: repeat-y; 
+	background-position: top left, top right; 
+}
+</style>
+
+<audio src="./sounds/planetready_bg.mp3" preload autoplay></audio>
+
+<!-- 로우 전체 시작 -->
 <div class="row" align="center">
-	<div class="col-md-2"><!--여백--></div>
+	<div class="col-xs-1"><!--여백--></div>
 
-    <!-- 중간부분 정렬로우 -->
-	<div class="col-md-8">
-     
-         <!-- 행성공략 - 행성상황 시작 -->
-		<div class="panel panel-success">
-            <div class="panel-heading">행성공략 - 행성상황</div>
-            <div class="panel-body" align="left">
-            	<h5>위치 : 갈릭토피아 은하계 알렉산드라 행성 북쪽지역 <br /><br />
-                    날씨 : 덥고 습함 / 40도 <br /><br />
-                    예상규모 : 30등급 보안시스템 작동예상</h5>
-                    
-                    <table class="table table-bordered">
-                    <tr align="center">
-                    <td class="success">무기시스템</td>
-                    <td>정찰불가</td>
-                    <td class="success">방어시스템</td>
-                    <td>정찰불가</td>
+    <!-- 행성공략 본격 시작 -->
+	<div class="col-xs-10">
+        <div class="panel panel-default width center">
+			<div class="panel-body center">
+            	<div class="alert alert-danger alert-dismissible" role="danger">
+                 <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                   <strong>행성공략</strong> - 워프준비 5, 4, 3, 2, 1, 0
+                </div>
+                <table class="table table-bordered center" style="width:930px;">
+                    <tr align="center" style="background-color:#FDEACC;">
+                    	<td colspan="4" height="30px">
+                        	<span style="color:#6C0; font-size:20px;"><strong>부관</strong> : <span style="font-size:16px;">[워프결과보고]</span>
+                             작전 완료, 행성 조사 완료</span>
+                        </td>
+                    </tr>
+                    <tr>
+                    	<td class="danger"><strong>발견한 행성</strong></td>
+                        <td>Jpub-3X 행성</td> <!-- 또는 이름모를 유저 행성 -->
+                        <td class="danger"><strong>타입</strong></td>
+                        <td>외계인</td> <!-- 또는 유저 -->
+                    </tr>
+                    <tr>
+                    	<td class="danger"><strong>적 지휘관</strong></td>
+                        <td>에스피스</td> <!-- 유저이름(갤럭시슝슝) -->
+                        <td class="danger"><strong>등급</strong></td>
+                        <td>3등급 외계인</td>
+                     </tr>
+                     <tr>
+                     	<td class="danger"><strong>적의 메시지</strong></td>
+                        <td colspan="3">(번역불가)</td> <!-- 또는 유저면 유저의 말 적어주기(기지말) -->
+                     </tr>
+                     <tr>
+                     	<td class="danger"><strong>적 규모</strong></td>
+                        <td colspan="3">정찰 불가능</td>
+                     </tr>
+                     <tr style="background-color:#FEABAB;">
+                     	<td colspan="4"><h5 style="color:#0000A0;">모에모에님! 전투에 돌입할까요?</h5></td>
+                     </tr>
+                     <tr align="center">
+                     	<td colspan="4">  
+                        	<!-- 케릭터 현황 -->
+                        	<table width="850px">
+                            	<tr>
+                                	<td width="300px">              
+                                    <!-- 체력부분 열기 -->
+                                    <div class="input-group">
+                                    <span class="input-group-addon"><span class="label label-danger">체력</span></span>
+                                        <div class="progress, form-control" style="background-color:#FEE;">
+                                        <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: <?=$hp_per?>%;" id="hp_per"><?=$hp_per?>%</div>
+                                        </div>
+                                        <span class="input-group-addon" id="hp_text"><?=$hp?> / <?=$maxhp?></span>
+                                    </div>
+                                    <!-- 체력부분 닫기 -->
+                                    <!-- 멘탈부분 열기 -->
+                                    <div class="input-group">
+                                    <span class="input-group-addon"><span class="label label-warning">멘탈</span></span>
+                                        <div class="progress, form-control" style="background-color:#FFE7D9;">
+                                        <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: <?=$mental_per?>%;" id="mental_per"><?=$mental_per?>%</div>
+                                        </div>
+                                        <span class="input-group-addon" id="mental_text"><?=$mental?> / <?=$mental?></span>
+                                    </div>
+                                    <!-- 멘탈부분 닫기 -->
+                                    </td>
+                                    <td width="20px"></td>
+                                	<td>
+                                    
+                                    <table width="500px" class="table table-bordered center">
+                                    	<tr>
+                                        	<td colspan="6" style="background-color:#FEEFFE">모에모에님의 장착 아이템</td>
+                                        </tr>
+                                    	<tr>
+                                        	<td>광선검</td>
+                                            <td>엘릭건</td>
+                                            <td>러브 티셔츠</td>
+                                            <td>러브바지</td>
+                                            <td>전투화</td>
+                                            <td>지식의 책</td>
+                                        </tr>
+                                    </table>
+                                    
+                                    </td>
+                                </tr>
+                            </table>
+                            <!-- 케릭터 현황 닫기 -->
+                		</td>
+                     </tr>
+					<tr>
+                    	<td colspan="4" style="background-color:#FFC">나의 준비된 유닛</td>
                     </tr>
                     <tr align="center">
-                    <td class="success">방어벽</td>
-                    <td>하이페*온 특수**벽</td>
-                    <td class="success">방공시스템</td>
-                    <td>***한 ** Lvl *4</td>
+                    	<table  align="center" style="width:600px;">
+                        	<tr align="center">
+                                <td><img src="./images/base_g_unit1.gif" alt="..." width="100px" height="100px"></td>
+                                <td><img src="./images/base_g_unit2.gif" alt="..." width="100px" height="100px"></td>
+                                <td><img src="./images/base_m_unit3.gif" alt="..." width="100px" height="100px"></td>
+                                <td><img src="./images/base_m_unit2.gif" alt="..." width="100px" height="100px"></td>
+                                <td><img src="./images/base_m_unit2.gif" alt="..." width="100px" height="100px"></td>
+                            </tr>
+                            <tr align="center">
+                                <td><img src="./images/base_g_unit1.gif" alt="..." width="100px" height="100px"></td>
+                                <td><img src="./images/base_g_unit2.gif" alt="..." width="100px" height="100px"></td>
+                                <td><img src="./images/base_m_unit3.gif" alt="..." width="100px" height="100px"></td>
+                                <td><img src="./images/base_m_unit2.gif" alt="..." width="100px" height="100px"></td>
+                                <td><img src="./images/base_m_unit2.gif" alt="..." width="100px" height="100px"></td>
+                            </tr>
+                        </table>
                     </tr>
-                    <tr align="center">
-                    <td class="success">보호막</td>
-                    <td>1,000</td>
-                    <td class="success">미정</td>
-                    <td>없음</td>
+                    <tr>
+                    	<td><br />
+                            <form class="form-inline" role="form">
+                        	<table align="center" style="width:600px;">
+                            	<tr align="center">
+                                	<td>1번</td>
+                                    <td>
+                                    <select class="form-control" id="one_unit">
+                                      <option>패스루트</option>
+                                      <option>일반병사</option>
+                                      <option>겔겔겔</option>
+                                      <option>기계유닛</option>
+                                      <option>헤헤헤유닛</option>
+                                    </select>
+                                    </td>
+                                    <td>2번</td>
+                                    <td>
+                                    <select class="form-control" id="two_unit">
+                                      <option>패스루트</option>
+                                      <option>일반병사</option>
+                                      <option>겔겔겔</option>
+                                      <option>기계유닛</option>
+                                      <option>헤헤헤유닛</option>
+                                    </select>
+                                    </td>
+                                    <td>3번</td>
+                                    <td>
+                                    <select class="form-control" id="three_unit">
+                                      <option>패스루트</option>
+                                      <option>일반병사</option>
+                                      <option>겔겔겔</option>
+                                      <option>기계유닛</option>
+                                      <option>헤헤헤유닛</option>
+                                    </select>
+                                    </td>
+                                </tr>
+                                <tr align="center">
+                                	<td>4번</td>
+                                    <td>
+                                    <select class="form-control" id="four_unit">
+                                      <option>패스루트</option>
+                                      <option>일반병사</option>
+                                      <option>겔겔겔</option>
+                                      <option>기계유닛</option>
+                                      <option>헤헤헤유닛</option>
+                                    </select>
+                                    </td>
+                                    <td>5번</td>
+                                    <td>
+                                    <select class="form-control" id="five_unit">
+                                      <option>패스루트</option>
+                                      <option>일반병사</option>
+                                      <option>겔겔겔</option>
+                                      <option>기계유닛</option>
+                                      <option>헤헤헤유닛</option>
+                                    </select>
+                                    </td>
+                                    <td colspan="2">.</td>
+                                </tr>
+							</table>
+                         </td>
                     </tr>
-                    <tr align="center">
-                    <td class="success">행성골드</td>
-                    <td>정찰불가</td>
-                    <td class="success">행성가스</td>
-                    <td>정찰불가</td>
-                    </tr>
-                    </table>
-                    <div align="center">
-                    <img src="./images/char1.gif" /> <!-- 임시 --><br />
-                    <h4>행성 기지 주인 : 레알사나이(User)</h4>
-                    행성 메시지 : 사용자 지정 메시지임. 반가워요! 오기만 해봐! 피토할꺼임!ㅋㅋ<br />
-                    </div> 
-            </div>
-           	</div>
-    <!-- 행성공략 - 행성상황 종료 -->
+                    <tr>
+                    	<td><br />
+                        	<table align="center" style="width:650px;">
+                            	<tr>
+                                	<td>아이템장착&nbsp;</td>
+                                    <td>1번</td>
+                                    <td>
+                                    <select class="form-control" id="one_item">
+                                      <option>새살쏙쏙연고</option>
+                                      <option>마술피리</option>
+                                      <option>행성폭격 무전기</option>
+                                      <option>레알의 레알</option>
+                                      <option>온리유 수정구슬</option>
+                                    </select>
+                                    </td>
+                                    <td>&nbsp;2번</td>
+                                    <td>
+                                    <select class="form-control" id="two_item">
+                                      <option>새살쏙쏙연고</option>
+                                      <option>마술피리</option>
+                                      <option>행성폭격 무전기</option>
+                                      <option>레알의 레알</option>
+                                      <option>온리유 수정구슬</option>
+                                    </select>
+                                    </td>
+                                    <td>&nbsp;3번</td>
+                                    <td>
+                                    <select class="form-control" id="three_item">
+                                      <option>새살쏙쏙연고</option>
+                                      <option>마술피리</option>
+                                      <option>행성폭격 무전기</option>
+                                      <option>레알의 레알</option>
+                                      <option>온리유 수정구슬</option>
+                                    </select>
+                                    </td>
+                                </tr>
+                            </table>
+                         </td>
+                     </tr>
+                     <tr>
+                     	<td colspan="4"><br /><a href="./planetready.php"><button type="button" class="btn btn-warning">재도약 (300루비 소모)</button></a>&nbsp;
+                        				<button type="submit" class="btn btn-danger">공격!</button>
+                        </td>
+                     </tr>  
+                </table>
+                </form>
+			</div>
+		</div>
+	
+        	
+	</div>     
+    <!-- 행성공략 이제 닫기 -->
 
-         <!-- 특수아이템 효과 발동 시작 -->
-		<div class="panel panel-success">
-            <div class="panel-heading">특수아이템 효과 발동</div>
-            <div class="panel-body" align="center">
-            	<h5>발동! 장착한 특수아이템 합금도굴꾼장갑 +5 으로 인해 골드약탈 보너스가 10% 상승합니다.<br /><br />
-                    발동! 장착한 프리미엄 아이템 원샷! 불끈드링크 로 인해 멘탈이 매턴마다 50씩 회복됩니다.
-                    </h5>
-            </div>
-           	</div>
-		<!-- 특수아이템 효과 발동 종료 -->
-
-		<!-- 일반아이템 발동 예약 시작 -->
-		<div class="panel panel-success">
-            <div class="panel-heading">일반아이템 사용하기</div>
-            <div class="panel-body" align="center">
-				<form role="form" method="post" id="planet_ready" action="#">
-                <!-- 슬슬 여기서부터 아마 반복코드 필요성 제기 -->
-					<div class="form-group">
-						<div class="input-group">
-							<div class="input-group-addon">케릭터 포션</div>
-							<select class="form-control">
-                            	<option>사용안함</option>
-                                <option>호랑이연고</option>
-                                <option>멘탈포션(소)</option>
-                                <option>프리즘물약</option>
-                            </select>
-						</div>
-					</div>
-					<div class="form-group">
-						<div class="input-group">
-							<div class="input-group-addon">일반물품</div>
-							<select class="form-control">
-                            	<option>사용안함</option>
-                                <option>프리즘발생기</option>
-                                <option>멘탈아웃총</option>
-                                <option>사무라이의 정신</option>
-                            </select>
-						</div>
-					</div>
-            </div>
-           	</div>
-		<!-- 일반아이템 발동 예약 종료 -->
-
-
-         <!-- 공격순서 셋팅 -->
-		<div class="panel panel-success">
-            <div class="panel-heading">공격순서 설정</div>
-            <div class="panel-body" align="center">
-				<div class="form-group">
-					<div class="input-group">
-						<div class="input-group-addon">선택</div>
-                        <select multiple class="form-control">
-                          <option>대공포전용유닛</option>
-                          <option>정찰병 투입</option>
-                          <option>나노로봇 투입</option>
-                          <option>VS-3 미사일 공격</option>
-                          <option>(특수능력)원격폭격 지원</option>
-                        </select>
-						</div>
-					</div>
-                    <div class="input-group">
-						<div class="input-group-addon">확정</div>
-						<input class="form-control" id="attack1" type="text" placeholder="1번 공격 : 여기로" disabled><br />
-                        <input class="form-control" id="attack2" type="text" placeholder="2번 공격 : 여기로2" disabled><br />
-                        <input class="form-control" id="attack3" type="text" placeholder="3번 공격 : 여기로3" disabled><br />
-                        <input class="form-control" id="attack4" type="text" placeholder="4번 공격 : 여기로4" disabled><br />
-                        <input class="form-control" id="attack5" type="text" placeholder="5번 공격 : 여기로5" disabled><br />
-                        </form>
-						</div>
-					</div>
-            			<button type="submit" class="btn btn-danger">행성 공격!</button>&nbsp;
-                        <button type="submit" class="btn btn-warning">기지로 돌아가기</button> <br /><br />
-				</div>
-           	</div>
-			<!-- 공격순서 셋팅 종료 -->
-
-
-
-
+	<div class="col-xs-1"><!-- 공백 --></div>
 </div>
-<!-- 중간부분 정렬로우 종료 -->
-
-
-	<div class="col-md-2"><!-- 공백 --></div>
-</div>
-<!-- 윗 상단 로우 전체 닫기 -->
-
-
+<!-- 로우 전체 닫기 -->
 
 	<?
 		include_once "./common/footer.php";
